f7f7c606ee4094aac05b6d29532d175a7217b11b
Serge Hill <sergehill@gmail.com>
2012-11-12 00:00:46 +0300

    Rsa modinverse added

--- Crytpro/RSA.cs
+++ Crytpro/RSA.cs
@@ -96,5 +96,42 @@ namespace Cryptpro
 
 			return encodedResult;
 		}
+
+		private static BigInteger ModInverse(BigInteger a, BigInteger b)
+		{
+			BigInteger dividend = a % b;
+			BigInteger divisor = b;
+
+			BigInteger last_x = BigInteger.One;
+			BigInteger curr_x = BigInteger.Zero;
+
+			while (divisor.Sign > 0)
+			{
+				BigInteger quotient = dividend / divisor;
+				BigInteger remainder = dividend % divisor;
+				if (remainder.Sign <= 0)
+				{
+					break;
+				}
+
+				/* This is quite clever, in the algorithm in form
+				 * ax + by = gcd(a, b) we only keep track of the
+				 * value curr_x and the last_x from last iteration,
+				 * the y value is ignored anyway. After remainder
+				 * runs to zero, we get our inverse from curr_x */
+				BigInteger next_x = last_x - curr_x * quotient;
+				last_x = curr_x;
+				curr_x = next_x;
+
+				dividend = divisor;
+				divisor = remainder;
+			}
+
+			if (divisor != BigInteger.One)
+			{
+				throw new Exception("Numbers a and b are not relatively primes");
+			}
+			return (curr_x.Sign < 0 ? curr_x + b : curr_x);
+		}
 	}
 }
